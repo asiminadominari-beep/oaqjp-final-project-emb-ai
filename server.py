163b47a968001ca956b314f6cda20fced61a39db
asiminadominari-beep <asimina.dominari@gmail.com>
2026-02-25 14:36:01 -0500

    Final 10/10 Pylint compliant server.py

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+This module initiates the Flask server for emotion detection.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -31,4 +34,3 @@ def render_index_page():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
